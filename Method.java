9fb1e8bac500a20141b3fc5af5d425cad330b559
Yumiko <wakinghira73@gmail.com>
2021-08-24 17:44:02 +0900

    first commit

---
+++ Method.java
@@ -0,0 +1,13 @@
+public class Method {
+    public static void main(String[] args) {
+        // printDataメソッドを呼び出し
+        printDate();
+
+    }
+// printDataメソッドを定義
+    public static void printDate() {
+        System.out.println("私の名前はKate Jonesです");
+        
+    }
+    
+}
---
+++ Method02.java
@@ -0,0 +1,12 @@
+public class Method02 {
+    public static void main(String[] args) {
+        printData("Kate Jones");
+        printData("John Christopher Smith");
+    }
+// メソッド名（仮引数）　データ型　変数名
+    public static void printData(String name) {
+        System.out.println("私の名前は" + name + "です");
+        
+    }
+    
+}
